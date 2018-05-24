314d652c61804cea337a5392a33d79a604d55189
Fredrik Wollsén <fredrik@neam.se>
2018-05-24 12:37:57 +0300

    Attempt to implement browser.alarms but ultimately had to comment it to have the add-on work at all

--- src/background.js
+++ src/background.js
@@ -68,11 +68,21 @@ class StudyLifeCycleHandler {
    */
   async enableFeature(studyInfo) {
     console.log("enabling feature", studyInfo);
+    /*
     if (studyInfo.timeUntilExpire) {
-      browser.alarms.create(studyInfo.timeUntilExpire, () =>
-        browser.study.endStudy("expired"),
-      );
+      const alarmName = `${browser.runtime.id}:studyExpiration`;
+      const alarmListener = async alarm => {
+        if (alarm.name === alarmName) {
+          browser.alarms.onAlarm.removeListener(alarmListener);
+          await browser.study.endStudy("expired");
+        }
+      };
+      browser.alarms.onAlarm.addListener(alarmListener);
+      browser.alarms.create(alarmName, {
+        when: Date.now() + studyInfo.timeUntilExpire,
+      });
     }
+    */
     // Initiate our study-specific feature
     new Feature(studyInfo.variation, studyInfo.isFirstRun);
   }
