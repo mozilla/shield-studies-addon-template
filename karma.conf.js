d68e2b553c7df17b53a0399ab2e2509cf43c6004
Fredrik Wollsén <fredrik@neam.se>
2018-06-09 00:33:48 -0700

    Using spec.js suffix for unit tests

--- karma.conf.js
+++ karma.conf.js
@@ -38,7 +38,7 @@ module.exports = function(config) {
       "node_modules/sinon/pkg/sinon.js",
       "node_modules/sinon-chrome/bundle/sinon-chrome.min.js",
       "src/feature.js",
-      "test/unit/*.test.js",
+      "test/unit/*.spec.js",
     ],
     // preprocessors: { "src/feature.js": ["coverage"] },
     plugins: [
