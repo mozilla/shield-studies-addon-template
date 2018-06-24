14e9776335128207b67e92113b5842788cf468a7
Fredrik Wollsén <fredrik@neam.se>
2018-06-24 11:45:55 +0300

    The ending for ineligible should not trigger opening of a survey by default

--- src/studySetup.js
+++ src/studySetup.js
@@ -42,9 +42,7 @@ const baseStudySetup = {
       ],
     },
     ineligible: {
-      baseUrls: [
-        "https://qsurvey.mozilla.com/s3/Shield-Study-Example-Survey/?reason=ineligible",
-      ],
+      baseUrls: [],
     },
     expired: {
       baseUrls: [
