00adf95936e1a20ee8ae8baeabe742273c7c60fa
Fredrik Wollsén <fredrik@neam.se>
2019-02-18 10:09:00 +0200

    Added telemetry schema validation before attempting to submit ping

--- src/lib/synchronization.js
+++ src/lib/synchronization.js
@@ -75,7 +75,25 @@ class ModelSynchronization {
     const windowInfo = await browser.windows.getLastFocused()
 
     if (!windowInfo.incognito) {
-      browser.experiments.telemetry.submitPing(payload)
+      await browser.study.logger.debug("Telemetry about to be validated using browser.study.validateJSON");
+      const foo = await browser.study.validateJSON( payload,
+        {
+        	"type": "object",
+        	"properties": {
+        	  "frecency_scores": { "type": "array", "items": { "type": "number" } },
+        	  "model_version": { "type": "number" },
+        	  "loss": { "type": "number" },
+        	  "num_chars_typed": { "type": "number" },
+        	  "num_suggestions_displayed": { "type": "number" },
+        	  "rank_selected": { "type": "number" },
+        	  "study_variation": { "type": "string" },
+        	  "update": { "type": "array", "items": { "type": "number" } }
+          }
+        }
+      );
+      await browser.study.logger.debug(foo);
+      await browser.study.logger.debug("Telemetry about to be submitted using browser.experiments.telemetry");
+      await browser.experiments.telemetry.submitPing(payload)
       await browser.study.logger.debug("Telemetry submitted using browser.experiments.telemetry");
     }
   }
