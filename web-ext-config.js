2a6fd99d3eb7007f1cca642bf1b446fb0fdd8117
Fredrik Wollsén <fredrik@neam.se>
2019-02-18 14:50:54 +0200

    Developing and testing using dev edition since closer to the study target version

--- web-ext-config.js
+++ web-ext-config.js
@@ -10,7 +10,7 @@ const defaultConfig = {
     overwriteDest: true,
   },
   run: {
-    firefox: process.env.FIREFOX_BINARY || "nightly",
+    firefox: process.env.FIREFOX_BINARY || "firefoxdeveloperedition",
     browserConsole: true,
     startUrl: ["about:debugging"],
     pref: [
